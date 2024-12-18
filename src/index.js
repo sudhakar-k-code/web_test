d5bd70fc268172740462cc9abf38b05dad6b141c
sudhakar-k-code <sudhakar93.careers@gmail.com>
2024-12-18 21:10:27 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // The main component of your app
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root') // This links to the <div> in index.html
+);
+ 
